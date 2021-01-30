6bf0b525516bb413e7d38771dd8b7ca9c5a78df0
tadashi <tadashi.b@gmail.com>
2021-01-30 18:54:58 -0300

    Quiz, scores and integration with Next.js

---
+++ pages/quiz/[id].js
@@ -0,0 +1,10 @@
+import { func } from 'prop-types';
+import React from 'react';
+
+export default function QuizDaGaleraPage() {
+  return (
+    <div>
+      Desafio da próxima aula junto com animações
+    </div>
+  );
+}
