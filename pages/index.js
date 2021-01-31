6922213f938ce9ff3fa28761732bd63ffc91371a
tadashi <tadashi.b@gmail.com>
2021-01-31 00:29:22 -0300

    Last class

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
@@ -11,6 +12,7 @@ import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
+import Link from '../src/components/Link';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -34,7 +36,16 @@ export default function Home() {
       </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>Next Quiz</h1>
           </Widget.Header>
@@ -57,15 +68,49 @@ export default function Home() {
             </form>
           </Widget.Content>
         </Widget>
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da galera</h1>
-
-            <p>shfushfusfhusfhsuaf</p>
+            <ul>
+              {db.external.map((externalLink) => {
+                const [projectName, githubUser] = externalLink
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('vercel.app', '')
+                  .split('.');
+                return (
+                  <li key={externalLink}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
-
         </Widget>
-        <Footer />
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/btadashi" />
     </QuizBackground>
--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,10 +1,48 @@
-import { func } from 'prop-types';
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz';
 
-export default function QuizDaGaleraPage() {
+export default function QuizDaGaleraPage({ externalDb }) {
   return (
-    <div>
-      Desafio da próxima aula junto com animações
-    </div>
+    <ThemeProvider theme={externalDb.theme}>
+      <QuizScreen
+        externalQuestions={externalDb.questions}
+        externalBg={externalDb.bg}
+      />
+    </ThemeProvider>
+
+  // {/* <pre style={{ color: 'black' }}>
+  //   {JSON.stringify(externalDb.questions, null, 4)}
+  // </pre> */}
   );
+}
+
+/** Server-side rendering */
+export async function getServerSideProps(context) {
+  const [projectName, githubUser] = context.query.id.split('___');
+
+  try {
+    const externalDb = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+      .then((respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          return respostaDoServer.json();
+        }
+        throw new Error('Fail to get data');
+      })
+      .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+      .catch((err) => {
+        console.error(err);
+      });
+
+    console.log('externalDb', externalDb);
+
+    return {
+      props: {
+        externalDb,
+      },
+    };
+  } catch (err) {
+    throw new Error(err);
+  }
 }
